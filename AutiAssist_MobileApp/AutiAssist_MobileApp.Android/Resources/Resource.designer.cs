05acba11f355cfdda08a9de38ba2a70dbc6fec9d
pasindu <pasindu0996@gmail.com>
2021-04-27 05:45:28 +0530

    UI Optimizations

--- AutiAssist_MobileApp/AutiAssist_MobileApp.Android/Resources/Resource.designer.cs
+++ AutiAssist_MobileApp/AutiAssist_MobileApp.Android/Resources/Resource.designer.cs
@@ -15306,16 +15306,19 @@ namespace AutiAssist_MobileApp.Droid
 			public const int profile = 2131165337;
 			
 			// aapt resource value: 0x7F07009A
-			public const int test_custom_background = 2131165338;
+			public const int search_icon = 2131165338;
 			
 			// aapt resource value: 0x7F07009B
-			public const int tooltip_frame_dark = 2131165339;
+			public const int test_custom_background = 2131165339;
 			
 			// aapt resource value: 0x7F07009C
-			public const int tooltip_frame_light = 2131165340;
+			public const int tooltip_frame_dark = 2131165340;
 			
 			// aapt resource value: 0x7F07009D
-			public const int xamarin_logo = 2131165341;
+			public const int tooltip_frame_light = 2131165341;
+			
+			// aapt resource value: 0x7F07009E
+			public const int xamarin_logo = 2131165342;
 			
 			static Drawable()
 			{
