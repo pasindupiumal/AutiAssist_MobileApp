aa25fc9733063e6fd62e3d4ac67cdf0391bff2ec
pasindu <pasindu0996@gmail.com>
2021-04-29 09:08:04 +0530

    Optimizations

---
+++ AutiAssist_MobileApp/AutiAssist_MobileApp/Models/FrequentActivity.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutiAssist_MobileApp.Models
+{
+    public class FrequentActivity
+    {
+        [JsonProperty("autismAssistSEQ")]
+        public string AutismAssistSEQ { get; set; }
+
+        [JsonProperty("ageCategory1")]
+        public string AgeCategory1 { get; set; }
+
+        [JsonProperty("ageActivity1")]
+        public string AgeActivity1 { get; set; }
+
+        [JsonProperty("ageCategory2")]
+        public string AgeCategory2 { get; set; }
+
+        [JsonProperty("ageActivity2")]
+        public string AgeActivity2 { get; set; }
+
+        [JsonProperty("ageCategory3")]
+        public string AgeCategory3 { get; set; }
+
+        [JsonProperty("ageActivity3")]
+        public string AgeActivity3 { get; set; }
+    }
+}
--- AutiAssist_MobileApp/AutiAssist_MobileApp/ViewModels/HomeViewModel.cs
+++ AutiAssist_MobileApp/AutiAssist_MobileApp/ViewModels/HomeViewModel.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Xamarin.Essentials;
 
 namespace AutiAssist_MobileApp.ViewModels
 {
